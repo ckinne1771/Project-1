80958fff30c67ab3297b8ca2157a092fd70101df
ckinne1771 <ckinney1771@gmail.com>
2017-02-19 20:56:09 +0000

    Upload
    lolol

---
+++ Project1/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterScript : MonoBehaviour {
+
+    public int healthPoints;
+    public int attack;
+    public int defence;
+    public int magicAttack;
+    public int magicDefence;
+    public int speed;
+    public int luck;
+
+    public GameObject equippedWeapon;
+    public GameObject equippedArmour;
+    public GameObject equippedAccessory;
+    
+	
+	
+}
---
+++ Project1/Assets/Scripts/gameDataScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameDataScript : MonoBehaviour {
+
+    public GameObject characterInPlayer1Slot;
+    public GameObject characterInPlayer2Slot;
+    public GameObject characterInPlayer3Slot;
+
+
+}
